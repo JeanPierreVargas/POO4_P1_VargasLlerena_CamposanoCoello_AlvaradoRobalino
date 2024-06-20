6d0be4d8b4c3e205adb473f2a77b247c63d7118e
Angel Camposano <173110533+PAPOSANO@users.noreply.github.com>
2024-06-20 00:30:37 -0500

    creando clase usuario y autor

---
+++ src/Packages/Clases/Autor.java
@@ -0,0 +1,29 @@
+public Autor extends Usuario{
+    private String codigo, institucion, campoInvestigacion;
+    publi Autor(String nombre, String apellido, String correo, String institucion, String campoInvestigacion,'a'){
+        super(nombre,apellido,correo);
+        this.institucion=institucion;
+        this.campoInvestigacion=campoInvestigacion;
+        
+
+    }
+    public String getCodigo(){
+        return codigo;
+    }
+    public String getInstitucion(){
+        return institucion;
+    }
+     public String getCampoInvestigacion(){
+        return campoInvestigacion;
+    }
+    public setCodigo(codigo){
+        this.codigo=codigo;
+    }
+    public setInstitucion(institucion){
+        this.institucion=institucion;
+    }
+    public setCampoInvestigacion(campoInvestigacion){
+        this.campoInvestigacion=campoInvestigacion;
+    }
+
+}
