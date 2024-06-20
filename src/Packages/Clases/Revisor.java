2a5594b859507970ba910eac5096d8709a94c76b
Jean Vargas <173319164+JeanPierreVargas@users.noreply.github.com>
2024-06-20 01:21:13 -0500

    Clase Revisor

---
+++ src/Packages/Clases/Revisor.java
@@ -0,0 +1,50 @@
+package Packages.Clases;
+
+public class Revisor {
+    //ATRIBUTOS
+    private String accesoId;
+    private String password;
+    private String especialidad;
+    private int numeroArticulo; 
+
+    //CONSTRUCTOR
+    public Revisor(String accesoId, String password, String especialidad, int numeroArticulo){
+        this.accesoId = accesoId;
+        this.password = password;
+        this.especialidad = especialidad;
+        this.numeroArticulo = numeroArticulo;
+    
+    //MÉTODOS GETTERS Y SETTERS
+    }
+    public String getAccesoId(){
+        return accesoId;
+    }
+    public void setAccesoId(String accesoId){
+        this.accesoId = accesoId;
+    }
+
+
+    public String GetPassword(){
+        return password;
+    }
+    public void setPassword(String password){
+        this.password  =password;
+    }
+
+
+    public String getEspecialidad(){
+        return especialidad;
+    }
+    public void setEspecialidad(String especialidad){
+        this.especialidad = especialidad;
+    }
+
+
+    public int getNumeroArticulo(){
+        return numeroArticulo;
+    }
+    public void setNumeroEspecialidad(int numeroArticulo){
+        this.numeroArticulo = numeroArticulo;
+    }
+
+}
